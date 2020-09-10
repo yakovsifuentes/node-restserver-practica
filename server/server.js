b50053e34250f5ea60764a8863b089b2a66b059d
Rodrigo <yakov.sifuentes@gmail.com>
2020-09-09 20:11:58 -0500

    mi primer commit

---
+++ server/server.js
@@ -0,0 +1,40 @@
+require('./config/config');
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/usuario', function(req, res) {
+    res.json('get usuario')
+})
+
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+
+
+    res.json({
+        "persona": body
+    });
+})
+
+app.put('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+
+    res.json(`put usuario: ${id}`);
+})
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete usuario')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando pueto:', 3000);
+})
