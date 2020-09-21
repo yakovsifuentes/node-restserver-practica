3a473d80fb619b068195c1a0e1ba8eb0fd1cfad1
Rodrigo <yakov.sifuentes@gmail.com>
2020-09-21 18:44:36 -0500

    modelos,rutas

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,23 @@
 // ======================
 // Puerto
 // ======================
-
 process.env.PORT = process.env.PORT || 3000;
+
+// ======================
+// Entorno
+// ======================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ======================
+// Base de Datos
+// ======================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://yakovsifuentes:981lqG9jpwuGihSA@cluster0.00z5b.mongodb.net/cafe'
+}
+
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express')
+const mongoose = require('mongoose');
+
 const app = express()
 const bodyParser = require('body-parser');
 
@@ -10,30 +12,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario')
-})
-
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
+app.use(require('./routes/usuario'));
 
 
-    res.json({
-        "persona": body
-    });
-})
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (err, res) => {
+    if (err) throw err;
 
-app.put('/usuario/:id', function(req, res) {
+    console.log('Base de datos ONLINE');
+});
 
-    let id = req.params.id;
-
-    res.json(`put usuario: ${id}`);
-})
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario')
-})
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando pueto:', 3000);
